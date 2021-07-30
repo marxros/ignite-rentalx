4932165922d3024882295d9fe6037d67484f864c
Marx Roberto <marxmoita@gmail.com>
2021-07-29 23:52:48 -0300

    aplicando SOLID, e o singleton

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,27 +1,17 @@
-import { response, Router } from 'express';
+import { Router } from 'express';
 
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (req, res) => {
-
-  const { name, description } = req.body;
-
-  const categoryExists = categoriesRepository.findByName(name);
-
-  if (categoryExists) { return res.status(400).json({ error: 'Category already exists!' }) };
-  categoriesRepository.create({ name, description });
-
-  return res.status(201).send();
+categoriesRoutes.post("/", (request, response) => {
+  return createCategoryController.handle(request, response);
 });
 
-categoriesRoutes.get("/", (req, res) => {
-  const categories = categoriesRepository.list();
-
-  return res.json(categories);
+categoriesRoutes.get("/", (request, response) => {
+  return listCategoriesController.handle(request, response);
 });
-
 
 export { categoriesRoutes }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 
 const server = express();
 
 server.use(express.json());
 
-server.use('/categories', categoriesRoutes)
+server.use('/categories', categoriesRoutes);
+server.use('/specifications', specificationsRoutes);
 
-server.listen(3333,() => {
+server.listen(3333, () => {
   console.log('Server started on port 3333');
 });
