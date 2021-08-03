14c48659f4680c6f4e416174b28e794bbb3fbca3
Marx Roberto <marxmoita@gmail.com>
2021-08-02 21:33:32 -0300

    adicionando upload de arquivo com multer

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
+const app = express();
 
-const server = express();
+app.use(express.json());
+app.use(router)
 
-server.use(express.json());
-
-server.use('/categories', categoriesRoutes);
-server.use('/specifications', specificationsRoutes);
-
-server.listen(3333, () => {
+app.listen(3333, () => {
   console.log('Server started on port 3333');
 });
